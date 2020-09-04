7ed407715f6fdebedb708dd5d6282fca9a290816
AnSunPyo <ansunpyo.dev@gmail.com>
2020-09-04 12:50:56 +0900

    chapterEx - 20.09.04

---
+++ doitjava/src/Example/Arsterisk02.java
@@ -0,0 +1,15 @@
+package Example;
+
+public class Arsterisk02 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) { // ROW Çà, ÁÙ
+			for (int j = 0; j < i +2; j++) { // COLUMN ¿­, Ä­
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
